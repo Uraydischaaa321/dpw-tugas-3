ba6642c3118a53ba2193b7434ad6700057f804c6
Uraydischaaa321 <uraydicha@gmail.com>
2021-12-11 00:22:10 +0700

    Initial commit

---
+++ system/routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontview.home');
+});
+
+Route::get('home', function () {
+    return view('frontview.home');
+});
+
+Route::get('about', function () {
+    return view('frontview.about');
+});
+
+Route::get('vegetables', function () {
+    return view('frontview.vegetables');
+});
+
+Route::get('custumers', function () {
+    return view('frontview.custumers');
+});
+
+Route::get('contact', function () {
+    return view('frontview.contact');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('beranda', function () {
+    return view('backview.beranda');
+});
+
+Route::get('kategori', function () {
+    return view('backview.kategori');
+});
+
+Route::get('produk', function () {
+    return view('backview.produk');
+});
+
+Route::get('user', function () {
+    return view('backview.user');
+});
